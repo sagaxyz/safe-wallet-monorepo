395abe8cb57638f4450d6541fc92a9a32db707ef
Leoni Mella <leoni.mella@gmail.com>
2026-05-14 14:23:26 -0300

    feat(network-config): add saga network configuration

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,23 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/saga/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  SAFE_UTILS_SUPPORTED: false,
+  EXTRA_FOOTER_LINKS: [],
+  IS_LICENSED: false,
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '40px',
+    },
+    WELCOME: {
+      W: '200px',
+      H: '39px',
+    },
+  },
+} as TemplateConfig
+export default TEMPLATE_CONFIG
